80f14f6b26156ffb374f44a5797d579fa819ceb3
Susanta <inbox.susanta@gmail.com>
2019-01-07 22:30:29 +0530

    Signing Users Out

--- scripts/auth.js
+++ scripts/auth.js
@@ -12,9 +12,18 @@ signupForm.addEventListener('submit', (e) => {
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         // console.log(cred);
-        console.log(cred.user);
+        // console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
-    })
-})
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user signed out');
+    });
+});
