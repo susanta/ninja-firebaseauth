40a75c85967d5c62a275ee6831cf13db67125970
Susanta <inbox.susanta@gmail.com>
2019-01-08 21:05:21 +0530

    Logging Users In

--- scripts/auth.js
+++ scripts/auth.js
@@ -27,3 +27,22 @@ logout.addEventListener('click', (e) => {
         console.log('user signed out');
     });
 });
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+
+        // close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+})
