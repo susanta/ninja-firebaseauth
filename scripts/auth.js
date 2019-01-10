8b2985fa9ecc7ab0a4a0a3273a3a109bc4835dea
Susanta <inbox.susanta@gmail.com>
2019-01-10 21:32:41 +0530

    Tracking Auth Status

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,13 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    // console.log(user);
+    if (user) {
+        console.log('user logged in: ', user);
+    } else {
+        console.log('user logged out');
+    }
+});
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -23,9 +33,7 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('user signed out');
-    });
+    auth.signOut();
 });
 
 // login
@@ -38,7 +46,7 @@ loginForm.addEventListener('submit', (e) => {
     const password = loginForm['login-password'].value;
 
     auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
+        // console.log(cred.user);
 
         // close the login modal and reset the form
         const modal = document.querySelector('#modal-login');
