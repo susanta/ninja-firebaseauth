9df7a38d74d5b09e30c693002d68831dae50bd94
Susanta <inbox.susanta@gmail.com>
2019-01-10 22:23:50 +0530

    Getting Firestore Data

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,9 @@
+//  get data
+db.collection('guides').get().then(snapshot => {
+    // console.log(snapshot.docs);
+    setupGuides(snapshot.docs);
+});
+
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
     // console.log(user);
