6b0cff5c2ded38c8b2d6402c81e1bdb29f08656c
Susanta <inbox.susanta@gmail.com>
2019-01-16 21:05:29 +0530

    Firestore REaltime Listener

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,7 +4,7 @@ auth.onAuthStateChanged(user => {
     if (user) {
         // console.log('user logged in: ', user);
         //  get data
-        db.collection('guides').get().then(snapshot => {
+        db.collection('guides').onSnapshot(snapshot => {
             // console.log(snapshot.docs);
             setupGuides(snapshot.docs);
             setupUI(user);
